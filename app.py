7aeacbb0c9b1fd43dae60d08519109e95af0a008
mat-hoyoung <106013448+mat-hoyoung@users.noreply.github.com>
2022-06-22 14:30:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db.devsbook_back.drop()
 import requests
 from bs4 import BeautifulSoup
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+#베스트 셀러 크롤링
 data = requests.get('http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?mallGb=KOR&linkClass=33&range=0&kind=1&orderClick=DCb', headers=headers)
 bestseller_soup = BeautifulSoup(data.text, 'html.parser')
 
@@ -28,6 +29,7 @@ for bestseller_book in bestseller_books:
     }
     db.devsbook_bestseller.insert_one(doc)
 
+#프론트엔드북 크롤링
 front_data = requests.get('https://search.kyobobook.co.kr/web/search?vPstrKeyWord=react&searchPcondition=1&searchCategory=%EA%B5%AD%EB%82%B4%EB%8F%84%EC%84%9C@KORBOOK@@%EC%BB%B4%ED%93%A8%ED%84%B0/IT@33&collName=KORBOOK&from_CollName=%EC%A0%84%EC%B2%B4@UNION&vPstrTab=PRODUCT&from_coll=KORBOOK&currentPage=1&orderClick=LIZ', headers=headers)
 front_soup = BeautifulSoup(front_data.text, 'html.parser')
 front_books = front_soup.select('#search_gallery > tr > td')
@@ -47,6 +49,7 @@ back_data = requests.get('https://search.kyobobook.co.kr/web/search?vPstrKeyWord
 back_soup = BeautifulSoup(back_data.text, 'html.parser')
 back_books = back_soup.select('#search_gallery > tr > td')
 
+#백엔드북 크롤링
 for back_book in back_books:
     booktitle = back_book.select_one('div > div.title > a > strong').text
     price = back_book.select_one('div > div.price > div.sell_price > strong').text
@@ -58,7 +61,7 @@ for back_book in back_books:
     }
     db.devsbook_back.insert_one(doc)
 
-
+#메인페이지, jinja2
 @app.route('/', methods=["GET"])
 def main_page():
     bestseller_list = list(db.devsbook_bestseller.find({}, {'_id': False}))
@@ -71,4 +74,3 @@ def main_page():
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
-
